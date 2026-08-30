bec1bf6768d69a49ec1bb86df4f2419ef8d15410
Breno Alves Beirigo <brenobeirigo@gmail.com>
2026-08-30 06:39:49 +0200

    Align extension resource version

--- _extensions/case-study/case-study.lua
+++ _extensions/case-study/case-study.lua
@@ -84,7 +84,7 @@ function Pandoc(document)
   if quarto.doc.is_format("html") or FORMAT:match("html") then
     quarto.doc.add_html_dependency({
       name = "quarto-case-study",
-      version = "0.2.0",
+      version = "0.2.1",
       stylesheets = {"case-study.css"}
     })
     document.blocks = decorate_html_cases(document.blocks, document.meta)
